946675e6ee22d00ae642455e816c54b900153fa7
tao_chen <1157534259@qq.com>
2018-03-19 16:35:25 +0800

    mm

--- utils/constant.js
+++ utils/constant.js
@@ -1,6 +1,6 @@
 //url相关
-var BASE_URL = "http://gank.io/api";
-var GET_MEIZHI_URL = BASE_URL.concat("/data/%E7%A6%8F%E5%88%A9/10/");
+var BASE_URL = "https://gank.io/api";
+var GET_MEIZHI_URL = BASE_URL.concat("/data/福利/10/1");
 
 // 将方法、变量暴露出去
 module.exports = {
